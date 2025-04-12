353e6be0f8a871cae4c4a7a151358e0036ab0165
Rishabh Shukla <rs1394@students.untsystem.edu>
2025-04-12 00:18:46 -0500

    design update
    Frontend just got prettier

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/navbarStyles.css'; // We'll create this file
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-brand">Opportunet</Link>
-      <div className="navbar-links">
-        <Link to="/dashboard" className="nav-link">Browse Jobs</Link>
-        <Link to ="/chatbot" className="nav-link">Chatbot</Link>
-        <Link to="/login" className="nav-link">Login</Link>
-        <Link to="/signup" className="nav-link">Sign Up</Link>
+      <div className="nav-container">
+        <Link to="/" className="logo">
+          <span className="logo-text">Opportunet</span>
+          <span className="logo-dot">.</span>
+        </Link>
+        
+        <div className="nav-links">
+          <Link to="/dashboard" className="nav-link">Browse Jobs</Link>
+          <Link to="/chatbot" className="nav-link">Chatbot</Link>
+          <Link to="/login" className="nav-link login-link">Login</Link>
+          <Link to="/signup" className="nav-link signup-link">Sign Up</Link>
+        </div>
       </div>
     </nav>
   );
