bff6e11f157c043c8e3ee232f5e0893dd9297f7f
Rishabh Shukla <rs1394@students.untsystem.edu>
2025-10-20 02:22:21 -0500

    .
    .

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbarStyles.css'; 
-import logo from '../assets/logo.png'; 
+import logo from '../assets/logo.png';  
 
 const Navbar = () => {
   return (
