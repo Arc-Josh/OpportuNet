e3ed3297f9c272e7fc2cb3bb7d0d018922ccb014
willag0719 <willag0719@gmail.com>
2025-10-21 14:40:07 -0500

    gave chatbot memory and the ability to grab a users full name from the database

--- server/ai/chatbot.py
+++ server/ai/chatbot.py
@@ -4,19 +4,33 @@ from google import genai
 from google.genai import types
 import json
 from .personality import instructions
+from .toolkit import six_seven, six_or_seven, get_name, get_full_name
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
-def chatbot(dialogue:str):
+async def chatbot(dialogue:str,email:str,chat = None):
     client = genai.Client()
+
+    tools= types.Tool(function_declarations=[six_seven,get_name])
     config = types.GenerateContentConfig(
         system_instruction = instructions,
+        tools=[tools]
     )
-    response = client.models.generate_content(
-        model = 'gemini-2.5-flash',
-        contents=dialogue,
-        config = config,
+    response = chat
+    if chat is None:
+        chat = client.chats.create(model = "gemini-2.5-flash",config=config)
+        print("new chat memory...")
+        response = chat.send_message(message =f"User email: {email}\nQuestion: {dialogue}")
+    else:
+        response = chat.send_message(message =f"User email: {email}\nQuestion: {dialogue}")
+    part = response.candidates[0].content.parts[0]
+
+    if hasattr(part,"function_call") and part.function_call:
+        fn = part.function_call
+        if fn.name == "six_seven":
+            return six_or_seven(), chat
+        if fn.name == "get_name":
+            return await get_full_name(email), chat
         
-    )
-    return response.text
+    return response.text, chat
 
--- server/venv/bin/activate.fish
+++ server/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/willis/opnet/server/venv"
+set -gx VIRTUAL_ENV "/Users/willis/FINAL_CAP_CLONE/OpportuNet/server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
