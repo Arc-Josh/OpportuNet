7a3d30c94b6c7ecce87199e4f326994a8b0f0b9d
willag0719 <willag0719@gmail.com>
2025-10-21 19:36:09 -0500

    tried to fix memory of resume

--- server/ai/toolkit.py
+++ server/ai/toolkit.py
@@ -1,8 +1,17 @@
 from database import db
+from io import BytesIO
+from PyPDF2 import PdfReader
 
-get_name = {
-    "name":"get_name",
-    "description":"if a user asks to be identified, identify them by full name",
+def bytea_to_pdf_text(file_data: bytes) -> str:
+    reader = PdfReader(BytesIO(file_data))
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() or ""
+    return text
+
+get_info = {
+    "name":"get_info",
+    "description":"when a conversation is started, load the user info retrieved into the chat memory",
     "parameters":{
         "type":"object",
         "properties":{
@@ -14,17 +23,28 @@ get_name = {
     }
 }
 
-async def get_full_name(email:str)->str:
+#Here we got a simple async function that allows for the chatbot to retrieve our name from the database. 
+#Simple, yes, but I really do think that this concept can be expanded upon and be the differentiator in our project.
+
+async def get_user_info(email:str)->tuple[str,str]:
     try:
         connected = await db.connect_db()
         if connected:
             try:
                 name = await connected.fetchrow("""SELECT full_name FROM users WHERE email = $1""",email)
-                return name["full_name"]
+                resume= await connected.fetchrow("""SELECT file_data FROM resumes WHERE user_email = $1""",email)
+                resume_str = bytea_to_pdf_text(resume["file_data"])
+                return resume_str, name["full_name"]
+            
             except Exception as e:
                 print("failed")
                 return "failed"
+            finally:
+                connected.close()
 
     except Exception as e:
         print("failed to connect to the database with error code:",e)
         return "couldnt retrieve name"
+    finally:
+        connected.close()
+
